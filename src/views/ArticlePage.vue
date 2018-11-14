63551f2d1a38fffb74c5a11883e8be56a8773ab2
liyiwei <491027175@qq.com>
2018-11-14 17:55:50 +0800

    发送数据

--- src/views/ArticlePage.vue
+++ src/views/ArticlePage.vue
@@ -1,26 +1,52 @@
 <template>
 	<div class="article-page">
-		<form action="">
+		<form action="" method="post">
 			<div class="title">
-				<input type="text" placeholder="标题" name="title">
+				<input type="text" placeholder="标题" name="title" v-model="article.title">
 			</div>
 			<div class="tags">
-				<input type="text" placeholder="标签1" name="tag1">
-				<input type="text" placeholder="标签2" name="tag2">
-				<input type="text" placeholder="标签3" name="tag3">
+				<input type="text" placeholder="标签1" name="tag1" v-model="article.tags[0]">
+				<input type="text" placeholder="标签2" name="tag2" v-model="article.tags[1]">
+				<input type="text" placeholder="标签3" name="tag3" v-model="article.tags[2]">
 			</div>
-			<mavon-editor v-model="value"/>
-			<input type="submit" value="提交">
+			<mavon-editor v-model="article.content"/>
+			<button type="button" @click="sendData(article)">提交</button>
 		</form>
 	</div>	
 </template>
 <script>
 export default {
-	data() {
-		return {
-			value:''
-		}
-	}
+  data() {
+    return {
+      article: {
+        title: "",
+        tags: [],
+        content: ""
+      }
+    };
+  },
+  methods: {
+    sendData(params) {
+      let opts = {
+        method: "POST",
+        body: JSON.stringify(params),
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+      fetch("/fluttering/publicArticle", opts)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
 <style lang="less" scoped>
