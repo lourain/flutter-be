4c15cefae3afde8dbb1c61b9e2f4273e987db9e9
liyiwei <491027175@qq.com>
2018-11-27 18:35:18 +0800

    12313123

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="upload">
-		<form action="http://localhost:9999/upload" method="post" enctype="multipart/form-data" @submit="postData">
+		<form action="http://localhost:9999/upload" method="post" enctype="multipart/form-data" @submit="postData" ref="myform">
 			<input type="file" name="file1">
 			<input type="submit" value="上传" >
 		</form>
@@ -10,7 +10,7 @@
 	export default {
 		methods:{
 			postData(e) {
-				console.log(e);
+				console.log(this.$refs.myform);
 				e.preventDefault();
 				return false
 			}
