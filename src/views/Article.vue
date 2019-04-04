ac7bcf31dd058867b6590fb7f85eb3ab14334171
lyw <491027175@qq.com>
2019-04-04 17:15:28 +0800

    修改为confirm类型弹出框

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,25 +1,25 @@
 <template>
-	<div class="container">
-		<div class="article" v-for="(article,index) in articles" :key="index">
-			<h1>{{article.title}}</h1>
-			<div class="tags">
-				<el-tag v-for="(tag,index) in article.tags" :key="index">{{tag}}</el-tag>
-			</div>
-			<div class="ctrl">
-                <router-link :to="'/edit?id='+article._id">编辑</router-link>
-				<a class="remove" href="javascript:;" @click="remove(article._id)">删除</a>
-			</div>
-			<div class="time">{{article.time}}</div>
-			<div class="markdown" v-html="article.content"></div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="article" v-for="(article,index) in articles" :key="index">
+      <h1>{{article.title}}</h1>
+      <div class="tags">
+        <el-tag v-for="(tag,index) in article.tags" :key="index">{{tag}}</el-tag>
+      </div>
+      <div class="ctrl">
+        <router-link :to="'/edit?id='+article._id">编辑</router-link>
+        <a class="remove" href="javascript:;" @click="remove(article._id)">删除</a>
+      </div>
+      <div class="time">{{article.time}}</div>
+      <div class="markdown" v-html="article.content"></div>
+    </div>
+  </div>
 </template>
 <script>
 import request from "../request.js";
-const EXPIRED = 1111
+const EXPIRED = 1111;
 const marked = require("marked");
 let hljs = require("highlight.js");
-import "highlight.js/styles/atom-one-dark.css";
+import "highlight.js/styles/tomorrow.css";
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
@@ -50,26 +50,44 @@ export default {
     getArt() {
       request("GET", `/api/article/${location.search}`).then(res => {
         console.log(res);
-        if(res.code === EXPIRED){
-            return this.msgBox(res.msg)
+        if (res.code === EXPIRED) {
+          return this.msgBox(res.msg);
         }
         res.data.forEach(element => {
           element.content = marked(element.content || "", { sanitize: true });
         });
         this.articles = res.data;
       });
-	},
-	remove(id) {
-		request('DELETE',`/api/del/${id}`)
-			.then(res=>{
-				this.msgBox(res.msg)
-			})
-	},
-	 msgBox(msg) {
+    },
+    remove(id) {
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          request("DELETE", `/api/del/${id}`).then(res => {
+            this.$message({
+              type: "success",
+              message: "删除成功!",
+              onClose:action=>{
+                  this.$router.push("/");
+              }
+            });
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    msgBox(msg) {
       this.$alert(msg, "提示", {
         confirmButtonText: "确定",
         callback: action => {
-        	this.$router.push('/')
+          this.$router.push("/");
         }
       });
     }
@@ -77,43 +95,43 @@ export default {
 };
 </script>
 <style lang="less">
-	.container{
-		.article{
-			a{
-				text-decoration: none;
-				color: #666;
-			}
-			.tags,.time{
-				box-sizing: border-box;
-				padding: 0 2em;
-				text-align: left;
-				margin: 10px 0;
-			}
-			.ctrl{
-				display: flex;
-				justify-content: flex-end;
-				&>a{
-					display: block;
-					margin: 0 15px;
-					cursor: pointer;
-					&:hover{
-						color:cornflowerblue;
-					}
-				}
-			}
-			.markdown{
-				text-align: left;
-				color:#666;
-				pre{
-					margin: 15px 0;
-					padding: 5px 0;
-					background:rgba(0, 0, 0, .1);
-				}
-				p{
-					margin: 15px 0;
-				}
-			}
-			
-		}
-	}
+.container {
+  .article {
+    a {
+      text-decoration: none;
+      color: #666;
+    }
+    .tags,
+    .time {
+      box-sizing: border-box;
+      padding: 0 2em;
+      text-align: left;
+      margin: 10px 0;
+    }
+    .ctrl {
+      display: flex;
+      justify-content: flex-end;
+      & > a {
+        display: block;
+        margin: 0 15px;
+        cursor: pointer;
+        &:hover {
+          color: cornflowerblue;
+        }
+      }
+    }
+    .markdown {
+      text-align: left;
+      color: #666;
+      pre {
+        margin: 15px 0;
+        padding: 5px 0;
+        background: rgba(0, 0, 0, 0.1);
+      }
+      p {
+        margin: 15px 0;
+      }
+    }
+  }
+}
 </style>
